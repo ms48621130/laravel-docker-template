63cd1dd0408818f902866b81185283c38a95c78a
清水政起 <shimizumasaki@shimizuseikinoMacBook-Air.local>
2024-07-14 14:57:00 +0900

    add:ToDo論理削除機能の実装

--- src/app/Todo.php
+++ src/app/Todo.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Todo extends Model // Todoクラスを定義
 {
+    use SoftDeletes;
     protected $table = 'todos'; // app配下に作成されたTodoModelをデータベースのtodosテーブルとマッピングする。
     // 画面の入力項目が増減しても常に同じコードで登録処理が実現できるようになり保守性と可読性が向上する
     protected $fillable = [
